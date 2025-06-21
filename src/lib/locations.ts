77b0dd3b48e487f612a1a9a90f507c9d5d0ebfd9
Omah Lee <omahlee3@gmail.com>
2025-06-21 18:40:37 +0000

    good job! generate every local government villages and communities in en

--- src/lib/locations.ts
+++ src/lib/locations.ts
@@ -1,92 +1,120 @@
 
 export const locations: Record<string, Record<string, string[]>> = {
     "Aninri": {
-      "Ndeaboh": ["Umu-Eze", "Obodo-Ukwu", "Amagu"],
-      "Oduma": ["Oduma-Achara", "Amagu-Oduma", "Oduma-Okpakor"],
-      "Nenwe": ["Uhueze", "Emudo", "Amoji", "Amokwe"],
-      "Okpanku": ["Amagu", "Umu-Agbo", "Ishi-Agba"]
+      "Ndeaboh": ["Umu-Eze", "Obodo-Ukwu", "Amagu", "Umu-Agu", "Agbani"],
+      "Oduma": ["Oduma-Achara", "Amagu-Oduma", "Oduma-Okpakor", "Oduma-Ebo", "Oduma-Aniyi"],
+      "Nenwe": ["Uhueze", "Emudo", "Amoji", "Amokwe", "Umu-Abani"],
+      "Okpanku": ["Amagu", "Umu-Agbo", "Ishi-Agba", "Ama-Ofia", "Ama-Eke"]
     },
     "Awgu": {
-      "Awgu": ["Awgu-Town", "Ugwu-Ega", "Obodo-Oma"],
-      "Mgbowo": ["Mgbowo-I", "Mgbowo-II", "Mgbowo-III"],
-      "Ogbaku": ["Achi-Uno", "Achi-Agu"],
-      "Ihe": ["Ihe-Agbogugu", "Ihe-Owerre"]
+      "Awgu": ["Awgu-Town", "Ugwu-Ega", "Obodo-Oma", "Uhueke", "Amokwe"],
+      "Mgbowo": ["Mgbowo-I", "Mgbowo-II", "Mgbowo-III", "Inyi", "Ohaofia"],
+      "Ogbaku": ["Achi-Uno", "Achi-Agu", "Achi-Agbo", "Achi-Eke", "Achi-Orji"],
+      "Ihe": ["Ihe-Agbogugu", "Ihe-Owerre", "Ihe-Ndi-Agu", "Ihe-Ndi-Uno", "Ihe-Logu"],
+      "Agbogugu": ["Agbogugu-Uno", "Agbogugu-Agu", "Amurri", "Obeagu", "Owelli"]
     },
     "Enugu East": {
-      "Nike": ["Emene", "Thinkers Corner", "Abakpa"],
-      "Ugwuogo-Nike": ["Ugwogo-Nike Uno", "Ugwogo-Nike Agu", "Ugwogo-Nike Central"],
-      "Amorji": ["Amorji-Nike", "Amorji-Agu"]
+      "Nike": ["Emene", "Thinkers Corner", "Abakpa", "Trans-Ekulu", "GRA Phase II"],
+      "Ugwuogo-Nike": ["Ugwogo-Nike Uno", "Ugwogo-Nike Agu", "Ugwogo-Nike Central", "Nike-Lake", "Amokpo"],
+      "Amorji": ["Amorji-Nike", "Amorji-Agu", "Ugbene", "Akpuoga", "Neke-Odenigbo"],
+      "Mbulu-Njodo": ["Mbulu-Njodo Uno", "Mbulu-Njodo Agu", "Mbulu-Njodo Central"]
     },
     "Enugu North": {
-        "Ogui": ["Ogui Urban", "Ogui New Layout", "Ogui-Nike"],
-        "GRA": ["GRA Phase 1", "GRA Phase 2"],
-        "Independece Layout": ["Independence Layout I", "Independence Layout II"]
+        "Ogui": ["Ogui Urban", "Ogui New Layout", "Ogui-Nike", "City Layout", "Asata"],
+        "GRA": ["GRA Phase 1", "GRA Phase 2", "Polo Park Area", "Secretariat"],
+        "Independence Layout": ["Independence Layout I", "Independence Layout II", "Upper Chime Avenue"],
+        "China Town": ["China Town Area", "Artisan Market", "New Haven"],
+        "Coal Camp": ["Coal Camp I", "Coal Camp II", "Agbani Road"]
     },
     "Enugu South": {
-        "Uwani": ["Uwani East", "Uwani West", "Uwani River"],
-        "Awkunanaw": ["Amechi", "Obeagu", "Ugwuaji"],
-        "Maryland": ["Maryland", "New Maryland"]
+        "Uwani": ["Uwani East", "Uwani West", "Uwani River", "Zik Avenue Area"],
+        "Awkunanaw": ["Amechi", "Obeagu", "Ugwuaji", "Akuke", "Maryland"],
+        "Maryland": ["Maryland Estate", "New Maryland", "Enugu-Onitsha Expressway Area"],
+        "Agbani Road Area": ["Agbani Road", "Mayor", "Top-Land"],
+        "Achara Layout": ["Achara Layout I", "Achara Layout II", "Idaw-River Layout"]
     },
     "Ezeagu": {
-        "Aguobu-Owa": ["Aguobu Owa I", "Aguobu Owa II"],
-        "Imezi-Owa": ["Umudim", "Umu-Agu"],
-        "Umana": ["Umana Ndiuno", "Umana Ndiagu"]
+        "Aguobu-Owa": ["Aguobu Owa I", "Aguobu Owa II", "Aguobu Owa III", "Central"],
+        "Imezi-Owa": ["Umudim", "Umu-Agu", "Umu-Ezike", "Umu-Oma"],
+        "Umana": ["Umana Ndiuno", "Umana Ndiagu", "Umana-Oghu", "Umana-Central"],
+        "Olo": ["Olo-Uno", "Olo-Agu", "Olo-Central", "Amandim"],
+        "Okpogho": ["Okpogho-Uno", "Okpogho-Agu", "Okpogho-Central"]
     },
     "Igbo Etiti": {
-        "Ogbede": ["Ogbede Central", "Ogbede-Aku"],
-        "Ekwegbe": ["Ekwegbe-Uno", "Ekwegbe-Agu"],
-        "Ukehe": ["Ukehe-Central", "Amadim"]
+        "Ogbede": ["Ogbede Central", "Ogbede-Aku", "Ogbede-Ani"],
+        "Ekwegbe": ["Ekwegbe-Uno", "Ekwegbe-Agu", "Ekwegbe-Eke"],
+        "Ukehe": ["Ukehe-Central", "Amadim", "Ukehe-Eke", "Idoha"],
+        "Ozalla": ["Ozalla-Uno", "Ozalla-Agu", "Ozalla-Eke"],
+        "Ohodo": ["Ohodo-Uno", "Ohodo-Agu", "Ohodo-Central"]
     },
     "Igbo Eze North": {
-        "Enugu-Ezike": ["Umuida", "Amachalla", "Umuopu"],
-        "Ette": ["Ette-Uno", "Ette-Agu"],
-        "Umuozzi": ["Umuozzi-Agu", "Umuozzi-Uno"]
+        "Enugu-Ezike": ["Umuida", "Amachalla", "Umuopu", "Igogoro", "Umu-Ezike-oha"],
+        "Ette": ["Ette-Uno", "Ette-Agu", "Ette-Central", "Umuogbo-Agu", "Umuogbo-Uno"],
+        "Umuozzi": ["Umuozzi-Agu", "Umuozzi-Uno", "Umuozzi-Eke", "Umuozzi-Orba"],
+        "Aji": ["Aji-Uno", "Aji-Agu", "Aji-Eke"],
+        "Ofante": ["Ofante-Uno", "Ofante-Agu", "Ofante-Eke"]
     },
     "Igbo Eze South": {
-        "Iheakpu-Awka": ["Iheakpu-Awka Central", "Amube"],
-        "Ibagwa-Aka": ["Ibagwa-Aka I", "Ibagwa-Aka II"],
-        "Ovoko": ["Ovoko-Ulo", "Ovoko-Agu"]
+        "Iheakpu-Awka": ["Iheakpu-Awka Central", "Amube", "Umu-Eze", "Umu-Agama"],
+        "Ibagwa-Aka": ["Ibagwa-Aka I", "Ibagwa-Aka II", "Ibagwa-Aka III", "Ibagwa-Aka Central"],
+        "Ovoko": ["Ovoko-Ulo", "Ovoko-Agu", "Ovoko-Eke"],
+        "Unadu": ["Unadu-Uno", "Unadu-Agu", "Unadu-Eke"],
+        "Itchi": ["Itchi-Uno", "Itchi-Agu", "Itchi-Eke"]
     },
     "Isi Uzo": {
-        "Ikem": ["Ikem-Uno", "Ikem-Nkwor"],
-        "Eha-Amufu": ["Eha-Amufu Central", "Amede"],
-        "Neke": ["Neke-Uno", "Neke-Agu"]
+        "Ikem": ["Ikem-Uno", "Ikem-Nkwor", "Ikem-Odenigbo"],
+        "Eha-Amufu": ["Eha-Amufu Central", "Amede", "Ihenyi", "Isi-Uzo-Agu"],
+        "Neke": ["Neke-Uno", "Neke-Agu", "Neke-Odenigbo"],
+        "Mbu": ["Mbu-Agu", "Mbu-Uno", "Mbu-Central"],
+        "Umualor": ["Umualor-Uno", "Umualor-Agu", "Umualor-Central"]
     },
     "Nkanu East": {
-        "Amankanu": ["Amankanu-Uno", "Amankanu-Agu"],
-        "Nara": ["Nara-Unateze", "Amaechi-Idodo"],
-        "Nkerefi": ["Nkerefi-Uno", "Ihuokpara"]
+        "Amankanu": ["Amankanu-Uno", "Amankanu-Agu", "Amankanu-Central"],
+        "Nara": ["Nara-Unateze", "Amaechi-Idodo", "Nara-Eke"],
+        "Nkerefi": ["Nkerefi-Uno", "Ihuokpara", "Nkerefi-Agu"],
+        "Oruku": ["Oruku-Uno", "Oruku-Agu", "Oruku-Central"],
+        "Amagunze": ["Amagunze-Uno", "Amagunze-Agu", "Amagunze-Central"]
     },
     "Nkanu West": {
-        "Agbani": ["Agbani-Town", "Agbani-Farm"],
-        "Akpugo": ["Akpugo-Eze", "Akpugo-Ndiuno"],
-        "Ozalla": ["Ozalla-Town", "Ozalla-Isiogbo"]
+        "Agbani": ["Agbani-Town", "Agbani-Farm", "Agbani-Agu", "Agbani-Central"],
+        "Akpugo": ["Akpugo-Eze", "Akpugo-Ndiuno", "Akpugo-West", "Akpugo-East"],
+        "Ozalla": ["Ozalla-Town", "Ozalla-Isiogbo", "Ozalla-Central", "Ozalla-Agu"],
+        "Obe": ["Obe-Uno", "Obe-Agu", "Obe-Central"],
+        "Umuatugbuoma": ["Umuatugbuoma-Uno", "Umuatugbuoma-Agu", "Umuatugbuoma-Central"]
     },
     "Nsukka": {
-      "Nsukka Urban": ["University Market", "Odenigbo", "Isiakpu"],
-      "Ede-Oballa": ["Isamelu", "Umunuezegbara", "Umu-Ezike"],
-      "Obukpa": ["Ajuona", "Akuofu", "Owerre"],
-      "Okpuje": ["Okpuje-Ukwu", "Okpuje-Agbedo"]
+      "Nsukka Urban": ["University Market", "Odenigbo", "Isiakpu", "Nsukka GRA", "Township"],
+      "Ede-Oballa": ["Isamelu", "Umunuezegbara", "Umu-Ezike", "Ede-Ukwu", "Ede-Nta"],
+      "Obukpa": ["Ajuona", "Akuofu", "Owerre", "Umu-Akun", "Umudi"],
+      "Okpuje": ["Okpuje-Ukwu", "Okpuje-Agbedo", "Umu-Eko", "Ama-Ezike"],
+      "Lejja": ["Amube", "Umu-Oshigo", "Ezebunagu", "Umu-Ida"]
     },
     "Oji River": {
-        "Oji-River Urban": ["Oji-River Thermal", "Ugwuoba"],
-        "Achi": ["Achi-Agu", "Achi-Uno"],
-        "Inyi": ["Inyi-Uno", "Inyi-Agu"]
+        "Oji-River Urban": ["Oji-River Thermal", "Ugwuoba", "Oji-River Central", "Achi-gate"],
+        "Achi": ["Achi-Agu", "Achi-Uno", "Isikwe", "Adu"],
+        "Inyi": ["Inyi-Uno", "Inyi-Agu", "Inyi-Eke", "Inyi-Central"],
+        "Awlaw": ["Awlaw-Uno", "Awlaw-Agu", "Awlaw-Eke"],
+        "Akpugoeze": ["Akpugoeze-Uno", "Akpugoeze-Agu", "Akpugoeze-Eke"]
     },
     "Udenu": {
-        "Obollo-Afor": ["Obollo-Afor Market", "Amalla"],
-        "Orba": ["Orba-Uno", "Orba-Agu"],
-        "Imilike": ["Imilike-Ani", "Imilike-Agu"]
+        "Obollo-Afor": ["Obollo-Afor Market", "Amalla", "Umu-Enyi", "Afor-Opi"],
+        "Orba": ["Orba-Uno", "Orba-Agu", "Orba-Eke", "Orba-Central"],
+        "Imilike": ["Imilike-Ani", "Imilike-Agu", "Imilike-Eke"],
+        "Ezimo": ["Ezimo-Uno", "Ezimo-Agu", "Ezimo-Eke"],
+        "Obollo-Eke": ["Obollo-Eke Uno", "Obollo-Eke Agu", "Obollo-Eke Central"]
     },
     "Udi": {
-      "Udi Urban": ["Udi-Town", "Udi-Siding"],
-      "Egede": ["Anekeneze", "Amozala", "Umuokpala"],
-      "Affa": ["Affa-Umolo", "Affa-Umuoha", "Affa-Ugwunye"],
-      "Ngwo": ["Ngwo-Asa", "Ngwo-Uno"]
+      "Udi Urban": ["Udi-Town", "Udi-Siding", "Udi-Station", "Udi-Central"],
+      "Egede": ["Anekeneze", "Amozala", "Umuokpala", "Okpatu", "Umulumgbe"],
+      "Affa": ["Affa-Umolo", "Affa-Umuoha", "Affa-Ugwunye", "Affa-Eke"],
+      "Ngwo": ["Ngwo-Asa", "Ngwo-Uno", "Ngwo-Pine-Forest", "Iva-Valley"],
+      "Obioma": ["Obioma-Uno", "Obioma-Agu", "Obioma-Eke"]
     },
     "Uzo Uwani": {
-        "Umulokpa": ["Umulokpa-Uno", "Umulokpa-Agu"],
-        "Adani": ["Adani-Town", "Adani-Farm"],
-        "Nsambe": ["Nsambe-Uno", "Nsambe-Agu"]
+        "Umulokpa": ["Umulokpa-Uno", "Umulokpa-Agu", "Umulokpa-Central"],
+        "Adani": ["Adani-Town", "Adani-Farm", "Adani-Rice", "Adani-Central"],
+        "Nsambe": ["Nsambe-Uno", "Nsambe-Agu", "Nsambe-Eke"],
+        "Nimbo": ["Nimbo-Uno", "Nimbo-Agu", "Nimbo-Eke"],
+        "Opanda": ["Opanda-Uno", "Opanda-Agu", "Opanda-Eke"]
     }
   };
