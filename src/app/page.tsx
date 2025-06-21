54c3650ff00bed5d52865bf59517a71b29d58a58
Omah Lee <omahlee3@gmail.com>
2025-06-21 21:47:42 +0000

    i mean 123 cards in a row

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -416,7 +416,7 @@ export default function Home() {
                   </Button>
               </div>
               
-              <div className="grid grid-cols-1 gap-6">
+              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
                   {recentListings.map((ad) => (
                     <AdCard key={ad.id} {...ad} />
                   ))}
